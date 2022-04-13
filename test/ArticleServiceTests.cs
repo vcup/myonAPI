bf80e423e8d296426e220feff5b4c760ae279159
miryo <miryo@vcup.moe>
2022-04-13 09:08:58 +0800

    移动项目到 src 文件夹，同时新建单元测试项目

---
+++ test/ArticleServiceTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace APITest;
+
+public class ArticleServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
