d785bf42953af114b360ba6f8ee34a64a25cd2ba
miryo <miryo@vcup.moe>
2022-04-15 14:11:06 +0800

    在API返回的数据中使用 PascalCase 命名策略

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    // using Pascal Case Naming
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
